6da2ec231195a91a419048f4d45fc7cd4e5e020e
AndreiThira <andreithira03@gmail.com>
2023-11-16 14:36:52 +0000

    Progress towards completing userContext functionality

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
 const cors = require("cors")
+const cookieParser = require("cookie-parser")
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
@@ -13,6 +14,7 @@ const jwtSecret = process.env.JWT_SECRET;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use(cors({
     credentials:true,
@@ -23,13 +25,26 @@ app.get("/test", (req, res) => {
     res.json("test ok");
 })
 
+app.get("/profile", (req,res) =>{
+   const token = req.cookies?.token;
+if(token){
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        if (err) throw err;
+        res.json(userData)
+       })
+} else {
+    res.status(401).json("no token")
+}
+})
+
 app.post("/register", async (req,res) => {
     const {username, password} = req.body;
     const createdUser =  await User.create({username, password});
     jwt.sign({userID: createdUser._id}, jwtSecret, {}, (err, token)=>{
         if(err) throw err;
-        res.cookie("token", token).status(201).json({
+        res.cookie("token", token, {sameSite:"none", secure:true}).status(201).json({
             id: createdUser._id,
+            username
         });
     })
 })
